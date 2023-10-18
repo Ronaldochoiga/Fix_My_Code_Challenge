c8bfa02feba44f781467cb105a4608d397f00b52
ronaldochoiga <choigaronaldo@gmail.com>
2023-10-18 12:42:31 -0700

    documentation and code fixing

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,32 +1,33 @@
 #!/usr/bin/python3
-""" Module for the square class """
+""" Module for square class"""
+
 
 class Square():
-    """ Square class indicated """
+    """ Square class """
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
-        """ This is the instantiation of the class module """
+        """ Instantiation of class """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
-    def Permiter_of_my_square(self):
-        """ this functions calculates the perimeter of the square """
+    def permiter_of_my_square(self):
+        """ Perimeter of my square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
-        """ This is the printed rep """
+        """ Printable representation """
         return "{}/{}".format(self.width, self.height)
 
+
 if __name__ == "__main__":
-    """ creation of a square object """
-    s = Square(width = 12, height = 9)
+    """ Create a square object """
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.Permiter_of_my_square())
+    print(s.permiter_of_my_square())
--- 0x01-challenge/user.py
+++ 0x01-challenge/user.py
@@ -10,18 +10,11 @@ class User():
         """ Documentation """
         self.__email = None
 
-    @email.setter
-    def email(self, value):
-        """ Documentation """
-        if type(value) is not str:
-            raise TypeError("email must be a string")
-        self.__email = value
-
     @property
     def email(self):
         """ Documentation """
         return self.__email
-   
+
     @email.setter
     def email(self, value):
         """ Documentation """
@@ -29,6 +22,8 @@ class User():
             raise TypeError("email must be a string")
         self.__email = value
 
+  
+    
 if __name__ == "__main__":
 
     u = User()
